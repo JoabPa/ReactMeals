f26fef0d53bec209acd1d92913eea2393bbab9b6
JoabPa <joabparedes.ec@gmail.com>
2022-04-01 00:10:34 -0400

    meals component added with dummy data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Header from './Layout/Header';
 import React from 'react';
+
+import Header from './Layout/Header';
+import Meals from './Meals/Meals';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
