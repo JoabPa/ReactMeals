9fcf2cec78a0929f50777ad3981d71baf3356104
JoabPa <joabparedes.ec@gmail.com>
2022-04-01 22:04:30 -0400

    managing modal state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Header from './components/Layout/Header'
+import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const HideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <React.Fragment>
-      <Cart/>
-      <Header />
+      {cartIsShown && <Cart onClose={HideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import classes from './Cart.module.css';
 
-import Modal from '../UI/Modal'
+import Modal from '../UI/Modal';
 
 const Cart = (props) => {
   const cartItems = (
@@ -14,16 +14,18 @@ const Cart = (props) => {
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button-alt']}>Close</button>
+        <button className={classes['button-alt']} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
-      </div> 
+      </div>
     </Modal>
   );
 };
