8c11bb3b12ccbecdf8b140dd5fe001abf62effb2
JoabPa <joabparedes.ec@gmail.com>
2022-04-09 00:16:00 -0400

    sending orders to database

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -45,6 +45,13 @@ const Checkout = (props) => {
       enteredPostalCodeIsValid;
 
     if (!formIsValid) return;
+
+    props.onConfirm({
+      name: enteredName,
+      street: enteredStreet,
+      city: enteredCity,
+      postalCode: enteredPostalCode
+    });
   };
 
   const nameControlClasses = `${classes.control} ${
